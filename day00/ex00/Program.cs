9afd37ae98ea63a1441f9534ad49336c801f7cc8
Weasley Tandoori <wtandoor@ge-d6.kzn.21-school.ru>
2021-05-24 21:43:45 +0300

    sad

---
+++ day00/ex00/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace Program 
+{
+    class Program
+    {
+        static double  Annuity_payment(double sum, double rate, int term)
+        {
+            double annuity_payment;
+            double rate_per_month;
+            
+            rate_per_month = (rate / 12) / 100;
+            annuity_payment = (sum * rate_per_month * Math.Pow((1 + rate_per_month), (double) term)) /
+                              (Math.Pow((1 + rate_per_month), (double) term) - 1);
+            return (annuity_payment);
+        }
+
+        static double Percent(double rate, double reminder, int i)
+        {
+            double percent;
+            percent = reminder * rate * i / 100 / 365;
+            return (percent);
+        }
+
+        static double DecreaseInAmount(double sum, double rate, double payment, int selectedMonth, int term)
+        {
+            double sum_of_percents;
+            double annuity_payment;
+            double reminder;
+            double percent;
+            int    month;
+            DateTime now = DateTime.Now;
+
+            sum_of_percents = 0;
+            month = 0;
+            reminder = sum;
+            annuity_payment = Annuity_payment(sum, rate, term);
+            while (month != term)
+            {
+                percent = Percent(rate, reminder,(int) (now.AddMonths(month + 1) - now.AddMonths(month)).Duration().Days);
+                sum_of_percents += percent;
+                reminder = reminder - (annuity_payment - percent);
+                if (month == selectedMonth - 1)
+                {
+                    reminder -= payment;
+                    annuity_payment = Annuity_payment(reminder, rate, term - selectedMonth);
+                }
+                if (reminder <= 0)
+                    break;
+                month++;
+            }
+            return (sum_of_percents);
+        }
+
+        static int CountMonth(double reminder, double rate, double annuity_payment)
+        {
+            double rate_per_month;
+            int month;
+
+            rate_per_month = (rate / 12) / 100;
+            month = (int) Math.Log(annuity_payment / (annuity_payment - rate_per_month * reminder), 1 + rate_per_month);
+            return (month);
+        }
+
+        static double DecreaseInTerm(double sum, double rate, double payment, int selectedMonth, int term)
+        {
+            DateTime now = DateTime.Now;
+            double sum_of_percents;
+            double annuity_payment;
+            double reminder;
+            double percent;
+            int month;
+            int i;
+
+            i = 0;
+            sum_of_percents = 0;
+            month = term;
+            reminder = sum;
+            annuity_payment = Annuity_payment(sum, rate, term);
+            while (month != 0)
+            {
+                percent = Percent(rate, reminder,(int) (now.AddMonths(i + 1) - now.AddMonths(i)).Duration().Days);
+                sum_of_percents += percent;
+                reminder = reminder - (annuity_payment - percent);
+                if (month == term - selectedMonth + 1)
+                {
+                    reminder -= payment;
+                    month = CountMonth(reminder, rate, annuity_payment) + 1;
+                }
+                if (reminder <= 0)
+                    break;
+                month--;
+                i++;
+            }
+            return (sum_of_percents);
+        }
+        static void Output(double decrease_in_amount, double decrease_it_term)
+        {
+            double difference;
+            if (decrease_in_amount > decrease_it_term)
+            {
+                difference = Math.Round(decrease_in_amount, 2) - Math.Round(decrease_it_term, 2);
+                Console.WriteLine("Переплата при уменьшении платежа: {" + Math.Round(decrease_in_amount, 2) + "}р.");
+                Console.WriteLine("Переплата при уменьшении срока: {" + Math.Round(decrease_it_term, 2) + "}р.");
+                Console.WriteLine("Уменьшение срока выгоднее уменьшения платежа на {" + Math.Round(difference, 2) + "}р.");
+            }
+            else if (decrease_in_amount < decrease_it_term)
+            {
+                difference = Math.Round(decrease_it_term, 2) - Math.Round(decrease_in_amount, 2);
+                Console.WriteLine("Переплата при уменьшении платежа: {" + Math.Round(decrease_in_amount, 2) + "}р.");
+                Console.WriteLine("Переплата при уменьшении срока: {" + Math.Round(decrease_it_term, 2) + "}р.");
+                Console.WriteLine("Уменьшение платежа выгоднее уменьшения срока на {" + Math.Round(difference, 2) + "}р.");
+            }
+            else
+            {
+                Console.WriteLine("Переплата при уменьшении платежа: {" + Math.Round(decrease_in_amount, 2) + "}р.");
+                Console.WriteLine("Переплата при уменьшении срока: {" + Math.Round(decrease_it_term, 2) + "}р.");
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            double sum;
+            double rate;
+            double payment;
+            double decrease_in_amount;
+            double decrease_in_term;
+            int    term;
+            int    selectedMonth;
+            Double.TryParse(args[0], out sum); //сумма кредита
+            Double.TryParse(args[1], out rate); //процентная ставка
+            Double.TryParse(args[4], out payment); //сумма досрочного платежа
+            Int32.TryParse(args[2], out term); //количество месяцев кредита
+            Int32.TryParse(args[3], out selectedMonth); //номер месяца кредита, когда вносится досрочный платеж
+            decrease_in_amount = DecreaseInAmount(sum, rate, payment, selectedMonth, term);
+            decrease_in_term = DecreaseInTerm(sum, rate, payment, selectedMonth, term);
+            Output(decrease_in_amount, decrease_in_term);
+        }
+    }
+}
---
+++ day00/ex01/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace DictionarySearchName
+{
+	class Program
+	{
+		static int LevenshteinDistance(string s1, string s2)
+		{
+			int diff;
+			int[,] m = new int[s1.Length + 1, s2.Length + 1];
+			int i = 0;
+			int j = 0;
+			while (i <= s1.Length)
+			{
+				m[i, 0] = i;
+				i++;
+			}
+
+			while (j <= s2.Length)
+			{
+				m[0, j] = j;
+				j++;
+			}
+
+			i = 1;
+			while (i <= s1.Length)
+			{
+				j = 1;
+				while (j <= s2.Length)
+				{
+					diff = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
+					m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1, m[i, j - 1] + 1), m[i - 1, j - 1] + diff);
+					j++;
+				}
+
+				i++;
+			}
+
+			return m[s1.Length, s2.Length];
+		}
+
+		static void Main(string[] args)
+		{
+			int i;
+			int check;
+			int distance_levenstein;
+			string button;
+			string name;
+			string model_name;
+			string[] line = File.ReadAllLines("us.txt");
+			Console.WriteLine("Enter name:");
+			name = Console.ReadLine();
+			check = 0;
+			model_name = @"^[A-Za-z -]+$";
+			if (!Regex.IsMatch(name, model_name))
+			{
+				Console.WriteLine("Your name was not found.");
+				return;
+			}
+			while (check < 3)
+			{
+				i = 0;
+				while (i < line.Length)
+				{
+					distance_levenstein = LevenshteinDistance(name, line[i]);
+					if (distance_levenstein == check)
+					{
+						if (distance_levenstein == 0)
+						{
+							Console.WriteLine("Hello, " + line[i] + "!");
+							return;
+						}
+						Console.WriteLine("Did you mean \"" + line[i] + "\"Y/N");
+						button = Console.ReadLine();
+						if (button == "Y")
+						{
+							Console.WriteLine("Hello, " + line[i] + "!");
+							return;
+						}
+						else if (button != "N")
+							return;
+					}
+					i++;
+				}
+				check++;
+			}
+			Console.WriteLine("Your name was not found.");
+			return;
+		}
+	}
+}
